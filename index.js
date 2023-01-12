c2d372a9769aee8b7dca8580f83ae871719781c4
Camron <cawe219@gmail.com>
2023-01-12 00:25:49 -0500

    commit tree 9c13446980ebdf47a302697e867b37c09b5d7447

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+
+
+findMatching = (drivers, name) =>{
+    const findName = drivers.filter(drivers => name.toLowerCase() === drivers.toLowerCase());
+    return !!findName ? findName : [];
+}
+    
+
+
+
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter((posiblematch) => posiblematch.toLowerCase().indexOf(name.toLowerCase()) === 0);
+}
+
+function matchName (driver, name){
+    return driver.filter((record) => record.name === name);
+}
+
